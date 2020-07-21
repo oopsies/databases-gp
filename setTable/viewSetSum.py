c47148beb287c7f0f3a961e6561b8f5ac10226c3
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-07-21 14:33:30 -0500

    Price Calculation Fixed

--- setTable/viewSetSum.py
+++ setTable/viewSetSum.py
@@ -23,26 +23,9 @@ def viewSetSum():
             print("Invalid ID")
             set_id="none"
         else:
-            i = 0
-            mult=[0]*len(myresult)
-            prices=[0]*len(myresult)
-            newprices=0
-            mycursor.execute("SELECT Bricks.price FROM Set_t RIGHT JOIN Bricks ON Set_t.brick_id=Bricks.id WHERE set_id="+set_id)
-            myresult = mycursor.fetchall()
+            mycursor.execute("SELECT SUM(bricks_needed*price) FROM Set_t RIGHT JOIN Bricks ON Set_t.brick_id=Bricks.id WHERE set_id="+set_id)
+            myresult=mycursor.fetchall()
             for x in myresult:
-                prices[i]=x[0]
-                i=i+1
-            mycursor.execute("SELECT bricks_needed FROM Set_t RIGHT JOIN Bricks ON Set_t.brick_id=Bricks.id WHERE set_id="+set_id)
-            myresult = mycursor.fetchall()
-            i=0
-            for x in myresult:
-                mult[i]=x[0]
-                i=i+1
-            i=0
-            for x in range(0,4):
-                newprices=newprices+(mult[i]*prices[i])
-                i=i+1
-            print(set_id+"  has a set price of "+str(newprices))
-
+                print(set_id+" has a price of $"+str(x[0]))
 
 viewSetSum()
